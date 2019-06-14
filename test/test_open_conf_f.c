9a684abfb495b7f10fcdea9dcbe7c819d11001a1
shizzeer <chat00@wp.pl>
2019-06-15 00:26:10 +0200

    Change struct member reference to macro

--- test/test_open_conf_f.c
+++ test/test_open_conf_f.c
@@ -13,8 +13,6 @@
 
 #define WRAP_FOPEN_CORRECT 0x11111111
 
-struct conf config;
-
 FILE *__wrap_fopen(const char *path, const char *mode) {
 	/* Checks only one of the parameters because set_config_file_path function
 	   changes the value of path and it's hard to check this parameter inside the 
@@ -34,7 +32,7 @@ static void test_open_conf_f_success(void **state) {
 
 	will_return(__wrap_fopen, WRAP_FOPEN_CORRECT);
 
-	FILE *config_file = open_config_file("ports.conf", strlen("ports.conf"), &config);
+	FILE *config_file = open_config_file("ports.conf", strlen("ports.conf"));
 	assert_non_null(config_file);
 }
 
@@ -43,16 +41,11 @@ static void test_open_conf_f_failed(void **state) {
 
 	will_return(__wrap_fopen, WRAP_FOPEN_CORRECT);
 
-	FILE *config_file = open_config_file("wrong_file.conf", strlen("wrong_file.conf"), &config);
+	FILE *config_file = open_config_file("wrong_file.conf", strlen("wrong_file.conf"));
 	assert_null(config_file);
 }
 
-void preparing_to_test(struct conf *config) {
-	init_default_conf_dir("/home/shizzer/c/stoneshire/conf/", config);
-}
-
 int main() {
-	preparing_to_test(&config);
 
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(test_open_conf_f_success),
