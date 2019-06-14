59ca00a8d42005ebf0a2d3c3081b048fbbb54792
shizzeer <chat00@wp.pl>
2019-06-15 00:23:53 +0200

    Change main config dir struct member to macro

--- include/conf.h
+++ include/conf.h
@@ -1,15 +1,14 @@
 #ifndef _CONF_H_
 	#define _CONF_H_
 	#define PROP_LEN 50
+	#define CONF_DIR "/home/shizzer/c/stoneshire/conf/"
 
 	struct conf {
 		char property_from_file[PROP_LEN];
 		int property_value;
-		char conf_file_dir[35];	/* path to the configuration files */
 	};
 
-	FILE *open_config_file(const char *filename_with_ext, size_t filename_len, struct conf *config);
+	FILE *open_config_file(const char *filename_with_ext, size_t filename_len);
 	int fget_conf_value(FILE *conf_file, struct conf *config, const char *property);
-	void init_default_conf_dir(char *default_conf_dir, struct conf *config);
-	void set_config_file_path(char *config_file_path, const char *filename_with_ext, struct conf *config);
+	void set_config_file_path(char *config_file_path, const char *filename_with_ext);
 #endif
