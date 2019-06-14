1dd5006967d4329a0804fb49ba47f6beff51a1ff
shizzeer <chat00@wp.pl>
2019-06-15 00:23:25 +0200

    Change main config dir struct member to macro

--- conf/conf.c
+++ conf/conf.c
@@ -8,25 +8,19 @@
 #define CONFIG_PATH_LEN 33
 #define VALUE_NOT_FOUND -1
 
-FILE *open_config_file(const char *filename_with_ext, size_t filename_len, struct conf *config);
-int fget_conf_value(FILE *conf_file, struct conf *config, const char *property);
-void init_default_conf_dir(char *default_conf_dir, struct conf *config);
-
-void set_config_file_path(char *config_file_path, const char *filename_with_ext, struct conf *config);
-
 int fget_conf_value(FILE *conf_file, struct conf *config, const char *property) {
 	char *line = NULL;
 	size_t size_of_the_line = 0;
 
 	while (getline(&line, &size_of_the_line, conf_file) != -1) {
-		sscanf(line, "%s %d", config->property_from_file, &config->property_value);
-		if (ferror(conf_file)) {
+		if (sscanf(line, "%s %d", config->property_from_file, &config->property_value) == 2) {
+			if ((strcmp(config->property_from_file, property)) == 0) {
+				puts(property);
+				free(line);
+				return config->property_value;
+			}
+		} else if (ferror(conf_file)) {
 			print_error_and_exit("Configuration file read");
-		}
-
-		if ((strcmp(config->property_from_file, property)) == 0) {
-			free(line);
-			return config->property_value;
 		}
 	}
 
@@ -34,25 +28,21 @@ int fget_conf_value(FILE *conf_file, struct conf *config, const char *property) 
 	return VALUE_NOT_FOUND;
 }
 
-FILE *open_config_file(const char *filename_with_ext, size_t filename_len, struct conf *config) {
+FILE *open_config_file(const char *filename_with_ext, size_t filename_len) {
 	char *config_file_path = (char *)malloc(CONFIG_PATH_LEN + filename_len);	
 	if (config_file_path == NULL) {
 		print_error_and_exit("Malloc for config_file_path");
 	}
 
 
-	set_config_file_path(config_file_path, filename_with_ext, config);
+	set_config_file_path(config_file_path, filename_with_ext);
 
 	FILE *config_file = fopen(config_file_path, "r");
 	free(config_file_path);
 	return config_file;
 }
 
-void set_config_file_path(char *config_file_path, const char *filename_with_ext, struct conf *config) {
-	strncpy(config_file_path, config->conf_file_dir, (strlen(config->conf_file_dir) + 1));
+void set_config_file_path(char *config_file_path, const char *filename_with_ext) {
+	strncpy(config_file_path, CONF_DIR, (strlen(CONF_DIR) + 1));
 	strcat(config_file_path, filename_with_ext);
-}
-
-void init_default_conf_dir(char *default_conf_dir, struct conf *config) {
-	strncpy(config->conf_file_dir, default_conf_dir, (strlen(default_conf_dir) + 1));
 }
