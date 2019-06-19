19be5c5bf83e95aa3bf6f341f568118c37373785
shizzeer <chat00@wp.pl>
2019-06-19 23:47:27 +0200

    Listening socket init

--- src/main.c
+++ src/main.c
@@ -3,19 +3,30 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <stdbool.h>
+#include <netinet/in.h> // for sockaddr_in struct
+#include <stdint.h> // for types in sockets structs
+#include <arpa/inet.h> // for inet_ntop to test init_address_struct
 
 #include "err_handlers.h"
 #include "conf.h"
 
-static void server_init(int *listen_socket, struct conf *config);
+static void server_listening_init(int *listen_socket, struct conf *config, struct sockaddr_in *server_addr);
 static bool port_is_valid(struct conf *config);
+
+static void init_address_struct(struct sockaddr_in *server_addr, struct conf *config) {
+	memset(server_addr, 0, sizeof(struct sockaddr_in));
+	server_addr->sin_family = AF_INET;
+	server_addr->sin_port = htons(config->property_value);
+	server_addr->sin_addr.s_addr = INADDR_ANY;
+}
 
 int main() {
 
 	int listen_socket;
 	struct conf config;
+	struct sockaddr_in server_addr, client_addr;
 
-	server_init(&listen_socket, &config);
+	server_listening_init(&listen_socket, &config, &server_addr);
 
 	return 0;
 }
@@ -28,7 +39,8 @@ static bool port_is_valid(struct conf *config) {
 	}
 }
 
-static void server_init(int *listen_socket, struct conf *config) {
+static void server_listening_init(int *listen_socket, struct conf *config, struct sockaddr_in *server_addr) {
+	
 	*listen_socket = socket(AF_INET, SOCK_STREAM, 0);
 	if ((*listen_socket) < 0) {
 		print_error_and_exit("Creating listening socket");
@@ -45,11 +57,16 @@ static void server_init(int *listen_socket, struct conf *config) {
 		exit(EXIT_FAILURE);
 	}
 
-	/* -----------------------------------------------------------------------------------------------
-		KOLEJNY KROK TO KONFIGURACJA POBRANEGO PORTU, A KONKRETNIE ZAMIANA PORTU Z WARTOSCI SHORT NA 
-		WARTOSC "NETWORK BYTES" CZYLI TRZEBA ZAMIENIC WARTOSC Z DOMYSLNEGO LITTLE ENDIAN NA BIG
-		ENDIAN DZIEKI FUNKCJI htons
-		----------------------------------------------------------------------------------------------
-	*/
+	init_address_struct(server_addr, config);
+
+	int binding_error = bind(*listen_socket, (struct sockaddr *)server_addr, sizeof(*server_addr));
+	if (binding_error) {
+		print_error_and_exit("Binding listening socket");
+	}
+
+	if (listen(*listen_socket, 5) < 0) {
+		print_error_and_exit("Listen socket");
+	}
+
 	fclose(config_file);
 }
